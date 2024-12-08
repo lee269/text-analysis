9e8bad2bbd6b97b46c16f6ca0e02d6e3a8880025
David Lee <david@lee269.co.uk>
2024-12-08 13:34:54 +0000

    lda

--- R/ukfsr.R
+++ R/ukfsr.R
@@ -5,6 +5,11 @@ library(readr)
 library(here)
 library(tidytext)
 library(stringr)
+library(ggplot2)
+library(forcats)
+library(ukfsr)
+library(tm)
+library(topicmodels)
 
 read_folder <- function(infolder) {
   tibble(file = dir(infolder, full.names = TRUE)) %>%
@@ -39,4 +44,45 @@ wordcloud2::wordcloud2(data = freqs,
 
 wordcloud2::letterCloud(data = freqs, word = "FOOD")
 
+# by theme ---------------------------------------------------------------------
+read_folder(here("data")) |>
+  mutate(text = str_extract(text, "[a-zA-Z' ]+")) |> 
+  unnest_tokens(word, text) |> 
+  filter(!is.na(word)) |> 
+  filter(!word %in% c("td", "tr", "style", "class")) |> 
+  mutate(theme = str_extract(id, "T[1-5]")) |> 
+  select(theme, word) |> 
+  count(theme, word) |> 
+  bind_tf_idf(word, theme,n) |> 
+  group_by(theme) |> 
+  slice_max(tf_idf, n = 10) |> 
+  ungroup() |> 
+  ggplot(aes(tf_idf, fct_reorder(word, tf_idf), fill = theme)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~theme, ncol = 2, scales = "free") +
+  labs(x = "tf-idf", y = NULL) + 
+  scale_fill_manual(values = afcolours::af_colours(n = 5)) +
+  theme_ukfsr(base_size = 10)
 
+
+# dtm --------------------------------------------------------------------------
+
+
+x <- read_folder(here("data")) |>
+  mutate(text = str_extract(text, "[a-zA-Z' ]+")) |> 
+  unnest_tokens(word, text) |> 
+  filter(!is.na(word)) |> 
+  filter(!word %in% c("td", "tr", "style", "class", "figure", "source", "download", "image", "food", "data", "chart")) |> 
+  mutate(theme = str_extract(id, "T[1-5]")) |> 
+  select(theme, word) |> 
+  count(theme, word) |> anti_join(stop_words)
+
+dtm <- cast_dtm(x,term =  word,document =  theme,value =  n)
+
+theme_lda <- LDA(dtm, k = 5)
+
+topics <- tidytext::tidy(theme_lda)
+
+z <- topics |>
+  group_by(topic) |> 
+  slice_max(beta, n = 10)
